bb0476273c22d529feecf6722771afe53b122f5f
scup0o <haiphungw0@gmail.com>
2025-12-30 09:19:16 +0700

    connect note & book table

--- app/src/main/java/com/project/pooket/data/local/note/NoteEntity.kt
+++ app/src/main/java/com/project/pooket/data/local/note/NoteEntity.kt
@@ -1,12 +1,27 @@
 package com.project.pooket.data.local.note
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.ForeignKey.Companion.CASCADE
+import androidx.room.Index
 import androidx.room.PrimaryKey
+import com.project.pooket.data.local.book.BookEntity
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 
-@Entity(tableName = "notes")
+@Entity(
+    tableName = "notes",
+    foreignKeys = [
+        ForeignKey(
+            entity = BookEntity::class,
+            parentColumns = ["uri"],
+            childColumns = ["bookUri"],
+            onDelete = CASCADE
+        )
+    ],
+    indices = [Index(value = ["bookUri"])])
+
 data class NoteEntity(
     @PrimaryKey(autoGenerate = true) val id: Long = 0,
     val bookUri: String,
@@ -14,7 +29,7 @@ data class NoteEntity(
     val originalText: String,
     val noteContent: String,
     val timestamp: Long = System.currentTimeMillis(),
-    val colorHex: Long = 0xFFFFFF00, // Yellow default
+    val colorHex: Long = 0xFFFFFF00,
     val rectsJson: String,
 
     val textRangeStart: Int? = null,
@@ -41,7 +56,6 @@ data class NoteEntity(
     }
 }
 
-// Normalized Rectangle (0.0 to 1.0) relative to page width/height
 @Serializable
 data class NormRect(
     val left: Float,
